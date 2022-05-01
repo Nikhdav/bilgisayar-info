6106a2a44db4416dbedb9915240dc6881a0136f4
berke <82501420+Nikhdav@users.noreply.github.com>
2022-05-01 12:31:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import platform
+
+# İşletim Sistemi İçin
+print(platform.system())
+
+# İşlemci Bilgileri İçin
+print(platform.processor())
+
+# Python Versiyonu İçin
+print(platform.python_version())
+
+# Bilgisayarın Kullanıcı Adı İçin
+print(platform.node())
+
